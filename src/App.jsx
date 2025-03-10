94d93aa52c1a10beffde40c16456f8b3049e64b4
rach-syahputra <rach.syahputra@gmail.com>
2025-03-10 16:51:35 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import ButtonGradient from './assets/svg/ButtonGradient'
+
+const App = () => {
+  return (
+    <>
+      <div className='overflow-hidden pt-[4.75rem] lg:pt-[5.25rem]'></div>
+      <ButtonGradient />
+    </>
+  )
+}
+
+export default App
