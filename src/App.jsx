4f248889813e1f46ed6fb488fbae4c520a834ec7
rach-syahputra <rach.syahputra@gmail.com>
2025-03-11 20:20:59 +0800

    feat: add collaboration section

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import ButtonGradient from './assets/svg/ButtonGradient'
 
 import Benefits from './components/Benefits'
+import Collaboration from './components/Collaboration'
 import Header from './components/Header'
 import Hero from './components/Hero'
 
 const App = () => {
   return (
     <>
-      <div className='overflow-hidden pt-[4.75rem] lg:pt-[5.25rem]'>
+      <div className='overflow-hidden pb-20 pt-[4.75rem] lg:pt-[5.25rem]'>
         <Header />
         <Hero />
         <Benefits />
+        <Collaboration />
       </div>
       <ButtonGradient />
     </>
