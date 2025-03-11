8a61c7dfb675e0a400ac410676b1ce85e02586a2
rach-syahputra <rach.syahputra@gmail.com>
2025-03-12 00:07:56 +0800

    style: update collab icon sizes

--- src/components/Collaboration.jsx
+++ src/components/Collaboration.jsx
@@ -34,7 +34,7 @@ const Collaboration = () => {
             {collabText}
           </p>
 
-          <div className='relative left-1/2 flex aspect-square w-[22rem] -translate-x-1/2 -scale-75 rounded-full border border-n-6 md:scale-100'>
+          <div className='relative left-1/2 flex aspect-square w-[22rem] -translate-x-1/2 scale-75 rounded-full border border-n-6 md:scale-100'>
             <div className='m-auto flex aspect-square w-60 rounded-full border border-n-6'>
               <div className='m-auto aspect-square w-[6rem] rounded-full bg-conic-gradient p-[0.2rem]'>
                 <div className='flex h-full w-full items-center justify-center rounded-full bg-n-8'>
