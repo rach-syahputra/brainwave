95c28679c4c780e93de639262913822c690f9e4b
rach-syahputra <rach.syahputra@gmail.com>
2025-03-11 23:06:32 +0800

    feat: add pricing section

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,8 +1,11 @@
-const Heading = ({ title, text, className }) => {
+import Tagline from './Tagline'
+
+const Heading = ({ title, text, tag, className }) => {
   return (
     <div
       className={`${className || ''} mx-auto mb-12 max-w-[50rem] md:text-center lg:mb-20`}
     >
+      {tag && <Tagline className='mb-4 md:justify-center'>{tag}</Tagline>}
       {title && <h2 className='h2'>{title}</h2>}
       {text && <p className='body-2 mt-4 text-n-4'>{text}</p>}
     </div>
---
+++ src/components/Pricing.jsx
@@ -0,0 +1,55 @@
+import { smallSphere, stars } from '../assets'
+
+import { LeftLine, RightLine } from './design/Pricing'
+import Section from './Section'
+import Heading from './Heading'
+import PricingList from './PricingList'
+
+const Pricing = () => {
+  return (
+    <Section id='pricing' className='overflow-hidden'>
+      <div className='container relative z-2'>
+        <div className='relative mb-[6.5rem] hidden justify-center lg:flex'>
+          <img
+            src={smallSphere}
+            alt='Sphere'
+            width={255}
+            height={255}
+            className='relative z-1'
+          />
+          <div className='pointer-events-none absolute left-1/2 top-1/2 w-[60rem] -translate-x-1/2 -translate-y-1/2'>
+            <img
+              src={stars}
+              alt='Stars'
+              width={950}
+              height={400}
+              className='w-full'
+            />
+          </div>
+        </div>
+
+        <Heading
+          tag='Get started with Brainwave'
+          title='Pay once, use forever'
+        />
+
+        <div className='relative'>
+          <PricingList />
+          <LeftLine />
+          <RightLine />
+        </div>
+
+        <div className='mt-10 flex justify-center'>
+          <a
+            href='/pricing'
+            className='border-b font-code text-xs font-bold uppercase tracking-wider'
+          >
+            See the full details
+          </a>
+        </div>
+      </div>
+    </Section>
+  )
+}
+
+export default Pricing
